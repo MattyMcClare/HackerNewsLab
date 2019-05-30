6db00757647568dad948094f88f65274007c445e
Mateusz <matstanczak@gmail.com>
2019-05-30 13:44:04 +0100

    edfelkajsf

--- src/containers/StoryContainer.js
+++ src/containers/StoryContainer.js
@@ -13,56 +13,41 @@ class StoryContainer extends Component {
         };
         this.selectStory = this.selectStory.bind(this);
     }
-
-    componentDidMount() {
-        const url = "http://hacker-news.firebaseio.com/v0/topstories.json";
-        fetch(url)
-            .then(res => res.json())
-            .then((stories) => {
-                const latestStories = stories.slice(0, 20)
-                const promises = latestStories.map((id) => {
-                    return fetch(`http://hacker-news.firebaseio.com/v0/item/${id}.json`)
-                        .then(res => res.json())
-                });
-                Promise.all(promises)
-                    .then(storyObjects => {
-                        this.setState({ stories: storyObjects })
-                    })
-            })
+})
 
     }
 
-    selectStory(selectedIndex) {
-        const selectedStory = this.state.stories[selectedIndex];
-        this.setState({ selectedStory })
-        // const comments = selectedStory.kids
-        // const promises = comments.map((id) => {
-        //     return fetch(`http://hacker-news.firebaseio.com/v0/item/${id}.json`)
-        //         .then(res => res.json())
-        // });
-        // Promise.all(promises)
-        //     .then(commentObjects => {
-        //         this.setState({ comments: commentObjects })
-        //     })
-    }
+selectStory(selectedIndex) {
+    const selectedStory = this.state.stories[selectedIndex];
+    this.setState({ selectedStory })
+    // const comments = selectedStory.kids
+    // const promises = comments.map((id) => {
+    //     return fetch(`http://hacker-news.firebaseio.com/v0/item/${id}.json`)
+    //         .then(res => res.json())
+    // });
+    // Promise.all(promises)
+    //     .then(commentObjects => {
+    //         this.setState({ comments: commentObjects })
+    //     })
+}
 
 
 
-    render() {
+render() {
 
-        const commentList = this.state.comments ? <CommentList comments={this.state.comments} /> : null
-        return (
-            <div className='story-container'>
-                <h1>Hacker News</h1>
-                <StorySelector
-                    stories={this.state.stories}
-                    onStorySelected={this.selectStory}
-                />
-                <StoryDetail story={this.state.selectedStory} />
-                {commentList}
-            </div>
-        )
-    }
+    const commentList = this.state.comments ? <CommentList comments={this.state.comments} /> : null
+    return (
+        <div className='story-container'>
+            <h1>Hacker News</h1>
+            <StorySelector
+                stories={this.state.stories}
+                onStorySelected={this.selectStory}
+            />
+            <StoryDetail story={this.state.selectedStory} />
+            {commentList}
+        </div>
+    )
+}
 }
 
 export default StoryContainer;
